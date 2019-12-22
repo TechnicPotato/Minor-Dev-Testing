b3c1d0eb4134d63383c7c85666ce58aaa44316e7
Bill <ZhuangBill@users.noreply.github.com>
2019-12-22 20:26:08 +1100

    Added basic recieving functionality for Server.py

--- One Way Communication/Server.py
+++ One Way Communication/Server.py
@@ -1,6 +1,25 @@
 import socket
 
+# Globals
+HOST = ''
+PORT = 9001
 
 # Main Thread
 if __name__ == "__main__":
-    pass
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    # Threadlocks and waits for a connection
+    s.listen()
+    conn, addr = s.accept()
+    print("Connected to: {client}".format(conn))
+    while True:
+        # Recieve 1024 bytes, can change parameters to allow more
+        data = conn.recv(1024)
+        # Cannot recieve any data.
+        if data == 0:
+            break
+        data.decode("utf-8")
+        print(data)
+        conn.sendall(b"Message Recieved!")
+    conn.close()
+    
